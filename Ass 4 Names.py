dd511c6f2ed4e36f296320d9073a40393be5b919
jack1993ire <89884899+jack1993ire@users.noreply.github.com>
2021-09-29 23:31:03 -0700

    Add files via upload

---
+++ Ass 4 Names.py
@@ -0,0 +1,22 @@
+# This program is designed to take five names
+# It will then format those names as last name then first name
+# And finally print a nice list of those names
+
+# This is how to put in a name
+name = input("Please enter your list of names: ");
+
+# Then split each name into a list
+name_list = name.split('-')
+
+# Then format everything
+for x in name_list:
+    # Finally, make the output last name, first name
+    word = variable.split()
+
+    # And then give a way of choosing your names!
+    variable1 = word[0]
+    variable2 = word[1]
+
+    # Print outputs
+    print(variable2, variable1[0], ".")
+
