0b3fa5434b7fb3946bcd83389620f666e21d0cd2
jack1993ire <89884899+jack1993ire@users.noreply.github.com>
2021-09-29 18:06:31 -0700

    Add files via upload

---
+++ Ass 4  Average.py
@@ -0,0 +1,13 @@
+# This is a way of finding the average of five numbers
+# These are the numbers
+# Now we must tell python the numbers to use
+
+my_numbers= [500, 1500, 4500, 2000, 4000]
+
+#And then, just find the average
+# To do it, just divide the total of the numbers
+# By
+avg = sum(my_numbers) / len(my_numbers)
+
+print('The average of the numbers in the list is: ',avg )
+
